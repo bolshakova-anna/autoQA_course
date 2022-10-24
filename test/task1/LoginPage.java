b5a20f7e5e534a03bee3297169743aa27abd0a9d
bolshakova-anna <bolshakova.anna.s@yandex.ru>
2022-10-24 13:17:01 +0300

    update pageobejct pattern and xpaths

--- test/task1/LoginPage.java
+++ test/task1/LoginPage.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.junit.ScreenShooter;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Rule;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.disappears;
 import static com.codeborne.selenide.Configuration.*;
@@ -11,15 +12,14 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.addListener;
-import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
-public abstract class LoginPage {
+
+public class LoginPage {
 
   @Rule
   public ScreenShooter screenShooter = ScreenShooter.failedTests();
 
-  @BeforeClass
-  public static void openInbox() {
+  public LoginPage openInbox() {
     timeout = 10000;
     baseUrl = "https://ok.ru/";
     startMaximized = false;
@@ -27,22 +27,13 @@ public abstract class LoginPage {
     browserPosition = "890x10";
     browserSize = "780x950";
     addListener(new Highlighter());
-    
     open("/");
-    waitUntilPagesIsLoaded();
+    return this;
   }
 
-  protected static void waitUntilPagesIsLoaded() {
-    $(byText("Loading")).waitUntil(disappears, 20000);
-  }
-
-  @AfterClass
-  public static void logout() {
-    closeWebDriver();
-  }
 
   public static void login(String OKUsername,String OKPassword) {
-    $("#field_email.it.h-mod").val(OKUsername);
-    $("#field_password.it").val(OKPassword).pressEnter();
+    $(By.xpath("//*[@id='field_email']")).val(OKUsername);
+    $(By.xpath("//*[@id='field_password']")).val(OKPassword).pressEnter();
   }
 }
--- test/task1/LoginSpec.java
+++ test/task1/LoginSpec.java
@@ -1,5 +1,6 @@
 package task1;
 
+import org.junit.AfterClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
@@ -7,20 +8,24 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
-public class LoginSpec extends LoginPage {
-  private static String OKUsername = System.getProperty("OK.username", "technopolisbot");
-  private static String OKPassword = System.getProperty("OK.password", "technopolis16");
-  private static String OKName = System.getProperty("OK.idName", "technopolisbot technopolisbot");
-
-
+public class LoginSpec {
+  private static String OKUsername = System.getProperty("OK.username", "technoPol27");
+  private static String OKPassword = System.getProperty("OK.password", "technoPolis2022");
+  private static String OKName = System.getProperty("OK.idName", "technoPol27 technoPol27");
 
   @Test
   public void LoginShowsCorrectName() {
-    login(OKUsername,OKPassword);
-    $(By.xpath("//*[@id=\'hook_Block_Navigation\']/div/div/div[1]/a/div"))
+    LoginPage loginpage = new LoginPage();
+    loginpage.openInbox().login(OKUsername,OKPassword);
+    $(By.xpath("//div[@class='tico ellip']"))
             .shouldHave(text(OKName))
             .shouldBe(visible);
   }
 
+  @AfterClass
+  public static void logout() {
+    closeWebDriver();
+  }
 }
